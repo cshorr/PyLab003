b29f63b3e0d103772afc1344d03b6b23c3fce899
chris shortt <cshortt707@gmail.com>
2025-02-02 11:21:54 -0800

    First Commit

--- lab_chat.py
+++ lab_chat.py
@@ -3,13 +3,15 @@
 from pyre import Pyre
 from pyre import zhelper
 import zmq
+import socket
+import threading
 import uuid
 import json
 
 def get_peer_node(username):
     n = Pyre(username)
-    #n.set_header("CHAT_Header1","example header1")
-    #n.set_header("CHAT_Header2","example header2")
+    # n.set_header("CHAT_Header1","example header1")
+    # n.set_header("CHAT_Header2","example header2")
     n.start()
     return n
 
@@ -20,15 +22,11 @@ def join_group(node, group):
 def chat_task(ctx, pipe, n, group):
     poller = zmq.Poller()
     poller.register(pipe, zmq.POLLIN)
-    # print(n.socket())
     poller.register(n.socket(), zmq.POLLIN)
-    # print(n.socket())
-    while(True):
+    while True:
         items = dict(poller.poll())
-        # print(n.socket(), items)
         if pipe in items and items[pipe] == zmq.POLLIN:
             message = pipe.recv()
-            # message to quit
             if message.decode('utf-8') == "$$STOP":
                 break
             print(f"YOU: {message.decode('utf-8')}")
@@ -42,19 +40,12 @@ def chat_task(ctx, pipe, n, group):
                 case "SHOUT":
                     intended_group = cmds.pop(0).decode('utf-8')
                     if intended_group == group:
-                        # print(f"{peer_username}({peer_id}): {cmds}")
                         print(f"{peer_username}: {cmds.pop(0).decode('utf-8')}")
                 case "ENTER":
                     headers = json.loads(cmds.pop(0).decode('utf-8'))
-                    # print(f"NODE_MSG HEADERS: {headers}")
-                    # for key in headers:
-                    #    print("key = {0}, value = {1}".format(key, headers[key]))
-                    # print( f"{peer_username}({peer_id}): is now connected." )
-                    print( f"{peer_username}: is now connected." )
+                    print(f"{peer_username}: is now connected.")
                 case "JOIN":
-                    #print( f"{peer_username}({peer_id}): joined {cmds.pop(0).decode('utf-8')}." )
-                    print( f"{peer_username}: joined {cmds.pop( 0 ).decode( 'utf-8' )}." )
-            # print(f"NODE_MSG CONT: {cmds}")
+                    print(f"{peer_username}: joined {cmds.pop(0).decode('utf-8')}.")
     n.stop()
 
 def get_channel(node, group):
